5bfefb29128c04b6b4b333863823302f5fb7af7a
tan.le <phtan.lee@gmail.com>
2022-01-04 19:21:20 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Get express module:
 const express = require('express')
+// Get morgan
+const morgan = require('morgan')
 // initialize a new express() object:
 const app = express()
 const port = 3000
+
+app.use(morgan('combined'))
 
 // route:
 app.get('/', (req, res) => {

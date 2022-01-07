d0da380d6fc216cb279157fbcff67208beade7fc
tan.le <phtan.lee@gmail.com>
2022-01-07 18:05:47 -0500

    Completed fundamentals

--- src/index.js
+++ src/index.js
@@ -15,31 +15,47 @@ const port = 3000;
 // Source for all folders within PUBLIC:
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 // Template engine:
 app.engine(
-  "hbs",
-  // engine receives an object
-  // extname: to change the extension name for views
-  engine({
-    extname: ".hbs",
-  })
+    "hbs",
+    // engine receives an object
+    // extname: to change the extension name for views
+    engine({
+        extname: ".hbs",
+    })
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
+// Middleware:
+app.use(express.urlencoded({
+    extended: true,
+})); // Form sends normal HTML
+app.use(express.json()); // XMLHttpRequest, Fetch, axios sends json
+
 // route:
 app.get("/", (req, res) => {
-  // use HTTP to send response Hello World! to the client:
-  res.render("home");
+    // use HTTP to send response Hello World! to the client:
+    res.render("home");
 });
 
 app.get("/news", (req, res) => {
-  // use HTTP to send response Hello World! to the client:
-  res.render("news");
+    // use HTTP to send response Hello World! to the client:
+    res.render("news");
+});
+
+app.get("/search", (req, res) => {
+    // Get query parameters by req.query:
+    res.render("search");
+});
+
+app.post("/search", (req, res) => {
+    // Get query parameters by req.query:
+    res.send(`<h1>${req.body.q} - ${req.body.gender}</h1>`);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
